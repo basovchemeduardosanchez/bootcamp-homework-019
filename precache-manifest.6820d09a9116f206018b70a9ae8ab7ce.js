8716dafbc24e878c202ad14a793c37cf6cccc09d
Eduardo Sánchez Villafán <basov_chem_eduardo_sanchez@hotmail.com>
2020-08-24 23:09:21 -0500

    Updates

---
+++ precache-manifest.6820d09a9116f206018b70a9ae8ab7ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82db33f1fba9003038bd7a48126db2b",
+    "url": "/bootcamp-homework-019/index.html"
+  },
+  {
+    "revision": "f22a557a27114b984d87",
+    "url": "/bootcamp-homework-019/static/css/main.f315a23e.chunk.css"
+  },
+  {
+    "revision": "6eeb858371c3b8ae0b39",
+    "url": "/bootcamp-homework-019/static/js/2.af113f5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bootcamp-homework-019/static/js/2.af113f5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22a557a27114b984d87",
+    "url": "/bootcamp-homework-019/static/js/main.9c74c562.chunk.js"
+  },
+  {
+    "revision": "06decdab7e8cca1ec0ab",
+    "url": "/bootcamp-homework-019/static/js/runtime-main.b59a1624.js"
+  }
+]);
